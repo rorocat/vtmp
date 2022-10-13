66bd457f2239bacedccb371ff69fb4ca36b4b5d6
ChenJingYuan <765618907@qq.com>
2022-10-13 09:48:52 +0800

    chore: 集成TMUI3.0.84组件库

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 import * as Pinia from 'pinia';
+import tmui from '/@/tmui';
 
 export function createApp() {
   const app = createSSRApp(App);
   app.use(Pinia.createPinia());
+  app.use(tmui)
   return {
     app,
   };
---
+++ src/theme/index.ts
@@ -0,0 +1,6 @@
+export const theme = {
+ 	// 这里输入你定义的主题主色
+	//比如："primary": "#FF0000"
+	//名称如果与自带主题相同，将会覆盖。参考：https://tmui.design/doc/spec/主题色彩.html
+	
+ }
